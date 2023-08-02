a38d106fb8c55bc94cb8fe6d2cffcd6740da187a
pampam01 <emailhpwiko@gmail.com>
2023-08-03 00:16:45 +0700

    fix: some bug

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ let App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="  bg-cover bg-no-repeat bg-center" >
           <Navbar />
           <Hero />
         </div>
